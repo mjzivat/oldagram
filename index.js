b39763b0348a5b77d866701fa3f322d1ba071383
mjzivat <mjzivat@gmail.com>
2023-02-18 13:35:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const posts = [
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
+        avatar: "oldagram/images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21
